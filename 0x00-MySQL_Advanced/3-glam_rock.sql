6e73a7b3a13b0f1842bf2f4ab4a10561bf6fa6f4
OmarKHDR <omarsliman37@gmail.com>
2024-10-20 17:19:18 +0300

    span by me

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,7 @@
--- Glam Rock Fr
-SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan FROM metal_bands
-WHERE style LIKE '%glam rock%' AND (split <= 2022 OR split IS NULL)
+-- hello world please answer my call
+-- lists all bands with Glam rock as their main style,
+-- ranked by their longevity
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan 
+FROM metal_bands
+WHERE style LIKE '%glam rock%'
 ORDER BY fans DESC;
