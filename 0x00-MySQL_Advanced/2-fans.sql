adde95f80245a3ddceb642ff0c203520129e5564
OmarKHDR <omarsliman37@gmail.com>
2024-10-20 16:35:11 +0300

    fans by me

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,6 @@
--- A script to sort and count
-SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+-- hello world
+--  ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT origin, sum(fans) AS nb_fans
+FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
