082e926091f57fad575ac9708e4657d2bd538015
OmarKHDR <omarsliman37@gmail.com>
2024-10-24 03:52:21 +0300

    hello my world

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
-"""
-Implementing an expriting web cache and tracker
-"""
-import requests
+""" Module for Implementing an expiring web cache and tracker """
+
+from functools import wraps
 import redis
-import time
+import requests
+from typing import Callable
+
+r = redis.Redis()
 
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting how many times a request
+    has been made """
+
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
 
 
+@count_requests
 def get_page(url: str) -> str:
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
     """
-    Fetches HTML content from URL
-    """
-    cached_html = redis_client.get(url)
-
-    if cached_html:
-        print(f"Returning cached content for {url}")
-        return cached_html.decode('utf-8')
-
-    response = requests.get(url)
-    html_content = response.text
-
-    redis_client.setex(url, 10, html_content)
-
-    count_key = f"count:{url}"
-    redis_client.incr(count_key)
-
-    print(f"Feteched new content for {url}")
-
-    return html_content
+    req = requests.get(url)
+    return req.text
