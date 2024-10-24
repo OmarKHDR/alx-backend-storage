e8d869237d3de3dc54939001d9dd35ac501a5837
OmarKHDR <omarsliman37@gmail.com>
2024-10-24 03:47:54 +0300

    hello my world

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url: str) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        # Increment the access count each time the function is called
+        redis_store.incr(f'count:{url}')
+
+        # Check if the result is cached
+        cached_result = redis_store.get(f'result:{url}')
+        if cached_result:
+            return cached_result.decode('utf-8')
+
+        # If not cached, fetch the result
+        result = method(url)
+
+        # Cache the result with an expiration time of 10 seconds
+        redis_store.setex(f'result:{url}', 10, result)
+
+        return result
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
