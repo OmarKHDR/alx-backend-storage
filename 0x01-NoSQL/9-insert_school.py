1c787740fe130e24cc55c5cf3528d417a2452d3b
OmarKHDR <omarsliman37@gmail.com>
2024-10-28 23:55:01 +0300

    9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#1/usr/bin/env python3
+'''This docs is fucked up
+'''
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Keyword  is  consistentciotrtrogjreg
+    '''
+    id = mongo_collection.insert_one(kwargs).inserted_id
+    return id
