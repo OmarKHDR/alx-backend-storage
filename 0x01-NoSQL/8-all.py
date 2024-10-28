4bba812ddbeb3790d6ba34cc81f6abf97ff0518e
OmarKHDR <omarsliman37@gmail.com>
2024-10-28 23:14:33 +0300

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyhthon3
+'''This is the summary line
+'''
+import pymongo
+
+
+def list_all(mongo_collection):
+    '''This is docs sumarry list_all
+    '''
+    for i in mongo_collection.find():
+        print(i)
