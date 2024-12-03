dadd446d2b1c9bc88b2475b0a61c79ddef7ce7c9
OmarKHDR <omarsliman37@gmail.com>
2024-12-03 13:01:27 +0200

    idfk

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,11 +7,11 @@ from typing import ByteString
 
 
 class Cache():
-	def __init__(self) -> None:
-		self._redis = redis.Redis()
-		self._redis.flushdb()
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-	def store(self, data: str | ByteString | float | int) -> str:
-		id = str(uuid.uuid4())
-		self._redis.set(id, data)
-		return id
+    def store(self, data: str | ByteString | float | int) -> str:
+        id = str(uuid.uuid4())
+        self._redis.set(id, data)
+        return id
