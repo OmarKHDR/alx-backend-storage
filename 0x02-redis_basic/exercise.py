dfb1efc057c855eee251cde10ed13edf4e3cf690
OmarKHDR <omarsliman37@gmail.com>
2024-12-03 13:23:59 +0200

    idfk

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, float, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ THis is a docDocDoc
         """
         id = str(uuid.uuid4())
